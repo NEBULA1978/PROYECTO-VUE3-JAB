027f41e2b5a4322d8e3eea82640bd6d4ac11268e
NEBULA1978 <rpascualey@gmail.com>
2023-03-08 22:50:03 +0100

    Creamos contador que empiece en 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-
+<h2>Cena{{ contador + 1 }}</h2>
 
 </template>
 
 <script setup>
-  
+  import {ref} from "vue"
+  import{productos} from "./datos.js"
+  const contador=ref(0)
 </script>
 
 <style scoped>
--- src/datos.js
+++ src/datos.js
@@ -1,29 +1,30 @@
-export const productos=[
+// Array con diferentes objetos:
+export const productos = [
   {
     nombre: "ervigio",
     precio: 110,
     finDeSemana: true
-  },{
+  }, {
     nombre: "ATANAGILDO",
     precio: 125,
     finDeSemana: false
-  },{
-    nombre: "LEOVIGILDO",      
+  }, {
+    nombre: "LEOVIGILDO",
     precio: 73,
     finDeSemana: true
-  },{
+  }, {
     nombre: "ATAULFO",
     precio: 69,
     finDeSemana: true
-  },{
+  }, {
     nombre: "SISEBUTO",
     precio: 105,
     finDeSemana: false
-  },{
+  }, {
     nombre: "TEODORICO",
     precio: 112,
     finDeSemana: true
-  },{
+  }, {
     nombre: "RECESVINTO",
     precio: 71,
     finDeSemana: false
