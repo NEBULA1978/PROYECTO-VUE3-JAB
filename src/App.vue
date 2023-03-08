cc9492729606cb013f733d31845e7cfb3d5976b5
NEBULA1978 <rpascualey@gmail.com>
2023-03-08 23:03:37 +0100

    COntador cuando llega a 7 empieza en 1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
 <h2>Cena{{ contador + 1 }}</h2>
-
+<button @:click="siguiente">Siguiente ({{ contador +1 }}/ {{total}})</button>
 </template>
 
 <script setup>
   import {ref} from "vue"
   import{productos} from "./datos.js"
   const contador=ref(0)
+  const total=productos.length;
+  const siguiente=()=>{
+    contador.value++
+    if (contador.value>=total){
+      contador.value=0;
+    }
+  }
 </script>
 
 <style scoped>
 
 </style>
+<!-- Minuto 55:14 -->
