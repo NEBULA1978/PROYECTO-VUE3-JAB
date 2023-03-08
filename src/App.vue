237c6ffd8eafa3db58d4c10a2ca447b402c08e4f
NEBULA1978 <rpascualey@gmail.com>
2023-03-08 23:13:22 +0100

    Añadimos el nombre del rey del array de objetos datos.js ponemos primera letra de reyyes en mayusculas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-<h2>Cena{{ contador + 1 }}
-  con el rey godo {{ productos[contador].nombre }}
+<h2>Cena {{ contador + 1 }}
+  con el rey godo {{ rey }}
 </h2>
 
 <button @:click="siguiente">Siguiente ({{ contador +1 }}/ {{total}})</button>
 </template>
 
 <script setup>
-  import {ref} from "vue"
+  import {ref,computed} from "vue"
   import{productos} from "./datos.js"
   const contador=ref(0)
   const total=productos.length;
@@ -17,6 +17,12 @@
       contador.value=0;
     }
   }
+  const rey=computed(()=>{
+    const elNombre=productos[contador.value].nombre.toLowerCase()
+    return  elNombre.substring(0, 1).toUpperCase() + elNombre.substring(1)
+  })
+   
+
 </script>
 
 <style scoped>
